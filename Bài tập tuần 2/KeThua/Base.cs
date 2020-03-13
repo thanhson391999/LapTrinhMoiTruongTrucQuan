7ce79ca123b6675777c7ea05340b6b0c058d36c5
thanhson391999 <55115530+thanhson391999@users.noreply.github.com>
2020-03-13 18:42:50 +0700

    haizz
    ^^

---
+++ Bài tập tuần 2/KeThua/Base.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeThua
+{
+    class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("In Base Constructor");
+            Console.ReadLine();
+        }
+        public virtual void Method1()
+        {
+            Console.WriteLine("In Base.Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            Console.WriteLine("In Base.Method2");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Bài tập tuần 2/KeThua/Derived.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeThua
+{
+    class Derived : Base
+    {
+        public Derived()
+        {
+            Console.WriteLine("Base.Method1");
+            Console.ReadLine();
+        }
+        public override void Method1()
+        {
+            Console.WriteLine("in derived Method1");
+            Console.ReadLine();
+        }
+        public Derived(string var)
+        {
+            Console.WriteLine("In Derived constructor.Constructor parameter value:{0}", var);
+            Console.ReadLine();
+        }
+        public new void Method2()
+        {
+            Console.WriteLine("In Derived.Method2");
+            Console.ReadLine();
+        }
+    }
+}
